3acb695edb6c2ef34babdde3f192b0a278123b43
hanwckf <my375229675@gmail.com>
2020-02-27 10:22:28 +0800

    openssl: add patches from upstream

--- extract.mk
+++ extract.mk
@@ -14,6 +14,7 @@ $(STAGEDIR)/$(Trojan_SRC):
 
 $(STAGEDIR)/$(OpenSSL_SRC):
 	tar -xf $(DL_DIR)/$(OpenSSL_SRC).tar.gz -C $(STAGEDIR)
+	for p in $$(ls patches/openssl/*.patch); do patch -p1 -d $(STAGEDIR)/$(OpenSSL_SRC) < $$p; done
 
 $(STAGEDIR)/$(Boost_SRC):
 	7z x -bsp0 -o$(STAGEDIR) $(DL_DIR)/$(Boost_SRC).7z
